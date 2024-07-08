65a6015f5bf6be27493fe2a954241fd69e736045
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-07-08 16:15:36 -0400

    Create send-email.ts

---
+++ inngest/functions/send-email.ts
@@ -0,0 +1,18 @@
+/*import { inngest } from 'inngest'; // Assuming your Inngest client is in a separate file
+import { Resend } from 'resend'
+
+inngest.createFunction(
+  { id: 'sendWelcomeEmail' },
+  {event: "user.created"}
+  async ({ event, step }) => {
+    console.log(event.data.email)
+    resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: 'rylowong8@gmail.com',
+      subject: 'Welcome',
+      html: '<p>Thank you for signing up to join our community!</p>'
+    });
+    return "Email sent!"
+  }
+);
+*/
