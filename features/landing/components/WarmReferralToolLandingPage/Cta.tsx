957e79b65e20e95b6a85c34d57cf0dfbf080dae1
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-07-10 14:45:09 -0400

    Update Cta.tsx

--- features/landing/components/WarmReferralToolLandingPage/Cta.tsx
+++ features/landing/components/WarmReferralToolLandingPage/Cta.tsx
@@ -21,24 +21,22 @@ export function Cta() {
   };
 
   return (
-    <section className="w-full bg-gray-800 py-12 md:py-24 lg:py-24 xl:py-24">
+    <section className="w-full bg-gray-800 py-12 md:py-24 lg:py-24 xl:py-24 flex justify-center">
       <div className="container px-4 md:px-6">
         <div className="grid items-center gap-6">
           <div className="flex flex-col justify-center space-y-4 text-center">
             <div className="space-y-6">
               <h2 className="bg-gradient-to-r from-white to-gray-400 bg-clip-text text-3xl font-bold tracking-tighter text-transparent sm:text-5xl xl:text-5xl/none">
-                Find key data in your email in 1/10th the time.
+                Find your most relevant contacts for warm leads.
               </h2>
               <p className="mx-auto max-w-[600px] text-zinc-200 dark:text-zinc-100 md:text-xl">
-                Get started and have AI do the work for you. It reads all the
-                newsetters you want to monitor, and alerts you with only
-                relevant information.
+                AI will analyze your target customers and your contacts 24/7 to find mutual connections.
               </p>
             </div>
             <div className="mx-auto w-full max-w-sm space-y-4">
               <form className="flex space-x-2" onSubmit={handleSubmit}>
                 <Input
-                  className="max-w-lg flex-1 border-gray-900 bg-gray-600 text-white placeholder:text-gray-300"
+                  className="max-w-lg flex-1 border-gray-900 bg-gray-600 text-white placeholder:text-gray-300 pl-4"
                   placeholder="Enter your email"
                   type="email"
                   value={email}
